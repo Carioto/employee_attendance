a7ccdf8a89f052bb87fae744b487664d19b1089e
Carioto <dcarioto114@outlook.com>
2025-03-07 21:33:54 -0500

    monthly summary report

--- reports/urls.py
+++ reports/urls.py
@@ -6,4 +6,5 @@ app_name = 'reports'  # This fixes the namespace issue
 urlpatterns = [
     path('', views.reports_home, name='reports_home'),
     path('weekly-compliance/', views.weekly_compliance_report, name='weekly_compliance_report'),
+    path('monthly-attendance/', views.monthly_attendance_summary, name='monthly_attendance_summary')
 ]
